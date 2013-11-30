d5d5cf6754237179629c34e099870bd451ad8b42
Tom Boutell <tom@punkave.com>
2013-11-30 08:51:45 -0500

    Initial Commit

---
+++ imagecrunch/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  imagecrunch
+//
+//  Created by Thomas Boutell on 11/30/13.
+//  Copyright (c) 2013 Thomas Boutell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
